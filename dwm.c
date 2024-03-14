e56d838c4e8cb199cf977681399b31be2d4b76d5
stefanj122 <stefan.jeftic122@gmail.com>
2024-03-14 19:19:12 +0100

    feat(cursorwarp): Mouse follow changing monitor focus

--- dwm.c
+++ dwm.c
@@ -870,6 +870,8 @@ focusnthmon(const Arg *arg)
 	unfocus(selmon->sel, 0);
 	selmon = m;
 	focus(NULL);
+	if (selmon->sel)
+		XWarpPointer(dpy, None, selmon->sel->win, 0, 0, 0, 0, selmon->sel->w/2, selmon->sel->h/2);
 }
 
 void
